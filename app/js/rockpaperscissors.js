5ad73d546ff1437772162749defb3536e85a7ad4
Austen Weinhart <austen.weinhart@gmail.com>
2015-03-14 16:10:34 -0700

    Unit 5 Homework

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -20,14 +20,14 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return "You chose " + move;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return "The computer chose " + move;
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) { 
@@ -66,5 +66,19 @@ function playToFive() {
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
     /* YOUR CODE HERE */
+    while (playerWins < 5 && computerWins < 5) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        var winner = getWinner(playerMove, computerMove);
+        if (winner === "player") {
+            playerWins +=1;
+        } else if (winner === "computer") {
+            computerWins +=1;
+        }
+        console.log("You chose " + playerMove + ", while the computer chose " + computerMove + " .");
+        console.log("This round's winner is " + winner + "!");
+        console.log("The final score is Player: " + playerWins + " Computer: " + computerWins +"." );
+    }
     return [playerWins, computerWins];
 }
+playToFive();
